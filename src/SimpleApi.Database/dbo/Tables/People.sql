b09e3cff911961dd4714205b7f40d5d768851d72
N1K0232 <ns.nicolasilvestri@libero.it>
2023-01-13 17:11:19 +0100

    imported database

---
+++ src/SimpleApi.Database/dbo/Tables/People.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[People] (
+    [Id]        UNIQUEIDENTIFIER NOT NULL DEFAULT newid(),
+    [FirstName] NVARCHAR (100)   NOT NULL,
+    [LastName]  NVARCHAR (100)   NOT NULL,
+
+    PRIMARY KEY (Id)
+);
+
